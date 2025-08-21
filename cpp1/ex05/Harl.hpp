a147d7c2507ba358b1cc3b17f7abb7753f8f0ce9
Roaa Abu Shreeha <rabu-shr@c2r2s2.42amman.com>
2025-08-21 19:26:50 +0300

    42cpp

---
+++ cpp1/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabu-shr <rabu-shr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 17:00:49 by rabu-shr          #+#    #+#             */
+/*   Updated: 2025/08/21 19:10:49 by rabu-shr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public :
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+#endif
